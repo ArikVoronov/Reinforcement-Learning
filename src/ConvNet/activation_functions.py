fix first activation, loss normalization

src/ConvNet/activation_functions.py
@@ -69,11 +69,16 @@ class ReLu2(ActivationBase):
 
 class Softmax(ActivationBase):
 
-    def __init__(self):
+    def __init__(self, subtract_max=True):
         super(Softmax, self).__init__()
+        self._subtract_max = subtract_max
 
     def forward(self, ctx, layer_input):
-        e = np.exp(layer_input)
+        if self._subtract_max:
+            e = np.exp(layer_input - np.max(layer_input, axis=0))
+        else:
+            e = np.exp(layer_input)
+
         e_sum = np.sum(e, axis=0)
         output = e / e_sum
 

src/ConvNet/layer_classes.py
@@ -54,21 +54,28 @@ class FullyConnectedLayer(LayerBase):
         # layer_sizes is a list, ls[1] is self length, ls[0] is previous layer
         if type(layer_sizes[0]) == list:
             layer_sizes[0] = np.prod(layer_sizes[0])
-        self.ls = layer_sizes
+        self.layer_sizes = layer_sizes
         self.dw = None
         self.db = None
         self._initialize_weights()
 
     def _initialize_weights(self):
-        signs = (2 * np.random.randint(0, 2, size=self.ls[1] * self.ls[0]) - 1).reshape(self.ls[1], self.ls[0])
-        var = np.sqrt(2 / self.ls[1])
-        self.w = var * 1 * signs * (np.random.randint(10, 1e2, size=self.ls[1] * self.ls[0]) / 1e2).reshape(
-            [self.ls[1], self.ls[0]])
-        self.b = np.zeros([self.ls[1], 1])
+        signs = (2 * np.random.randint(0, 2, size=self.layer_sizes[1] * self.layer_sizes[0]) - 1).reshape(
+            self.layer_sizes[1], self.layer_sizes[0])
+        var = np.sqrt(2 / self.layer_sizes[1])
+
+        self.w = var * 1 * signs * (
+                    np.random.randint(10, 1e2, size=self.layer_sizes[1] * self.layer_sizes[0]) / 1e2).reshape(
+            [self.layer_sizes[1], self.layer_sizes[0]])
+
+        bound = 1 / np.sqrt(self.layer_sizes[1])
+        self.w = bound * 2 * (np.random.rand(self.layer_sizes[1], self.layer_sizes[0]) - 0.5)
+
+        self.b = np.zeros([self.layer_sizes[1], 1])
 
     def forward(self, ctx, layer_input):
         if len(layer_input.shape) > 2:
-            layer_input = layer_input.reshape(self.ls[0], -1)
+            layer_input = layer_input.reshape(self.layer_sizes[0], -1)
         layer_output = np.dot(self.w, layer_input) + self.b
         ctx.save_for_backward(layer_input)
         return layer_output
@@ -76,7 +83,7 @@ class FullyConnectedLayer(LayerBase):
     def backward(self, ctx: Context, grad_output):
         layer_input = ctx.get_saved_tensors()
         if len(layer_input.shape) > 2:
-            layer_input = layer_input.reshape(self.ls[0], -1)
+            layer_input = layer_input.reshape(self.layer_sizes[0], -1)
 
         db = np.sum(grad_output, axis=1).reshape(self.b.shape[0], 1)
         dw = np.dot(grad_output, layer_input.T)

src/ConvNet/losses.py
@@ -23,28 +23,69 @@ class MSELoss(LossBase):
 
     def forward(self, ctx, target, layer_input):
         loss_per_class = (layer_input - target) ** 2
-        loss = np.mean(loss_per_class)
+        loss = np.sum(loss_per_class, axis=0)
+        loss = np.mean(loss)
 
         ctx.save_for_backward(layer_input, target)
         return loss
 
     def backward(self, ctx):
         layer_input, target = ctx.get_saved_tensors()
-        grad = 2 * (layer_input - target) / (layer_input.shape[0] * layer_input.shape[1])
+
+        number_of_classes = layer_input.shape[0]
+        number_of_samples = layer_input.shape[1]
+        grad = 2 * (layer_input - target)
+        grad /= number_of_samples
+        grad /= number_of_classes
         return grad
 
 
-class NLLLoss(LossBase):
+class NegativeLikelihoodLoss(LossBase):
+    """
+    The negative likelihood loss
+    """
+
     def __init__(self):
-        super(NLLLoss, self).__init__()
+        super(NegativeLikelihoodLoss, self).__init__()
 
     def forward(self, ctx, target, layer_input):
-        raise Exception()
-        loss_per_class = None
+        loss_per_class = -target * layer_input
 
         loss = np.mean(loss_per_class)
         ctx.save_for_backward(layer_input, target)
         return loss
 
     def backward(self, ctx):
-        raise Exception()
+        layer_input, target = ctx.get_saved_tensors()
+        number_of_classes = layer_input.shape[0]
+        number_of_samples = layer_input.shape[1]
+        grad = -target
+        grad /= number_of_samples
+        grad /= number_of_classes
+        return grad
+
+
+class NLLoss(LossBase):
+    """
+    The negative log likelihood loss
+    """
+
+    def __init__(self):
+        super(NLLoss, self).__init__()
+
+    def forward(self, ctx, target, layer_input):
+        loss_per_class = -target * np.log(layer_input)
+        loss = np.mean(loss_per_class, axis=0)
+
+        loss = np.mean(loss)
+        ctx.save_for_backward(layer_input, target)
+        return loss
+
+    def backward(self, ctx):
+        layer_input, target = ctx.get_saved_tensors()
+        number_of_classes = layer_input.shape[0]
+        number_of_samples = layer_input.shape[1]
+        grad = -target * 1 / layer_input
+        grad /= number_of_samples
+        grad /= number_of_classes
+        return grad

src/ConvNet/model.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from src.ConvNet.activation_functions import ReLu,Softmax
+from src.ConvNet.activation_functions import ReLu, Softmax, LinearActivation
 from src.ConvNet.layer_classes import FullyConnectedLayer, Context, InputLayer, LayerBase
-from src.ConvNet.losses import MSELoss
+from src.ConvNet.losses import NLLoss, MSELoss
 from src.ConvNet.optim import SGD
-from src.ConvNet.utils import standardize, make_one_hot_vector, train
+from src.ConvNet.utils import make_one_hot_vector, train, normalize, squish_range
 
 
 class Model:
@@ -52,13 +52,11 @@ class Model:
 
 def make_example_net():
     input_size = 784
-    layer_sizes = [200, 10]
+    layer_sizes = [20, 10]
 
-    loss = MSELoss()
-
+    loss = NLLoss()
     activation_list = [ReLu, Softmax]
-
-    layers_list = [FullyConnectedLayer((input_size, layer_sizes[0]))]
+    layers_list = [FullyConnectedLayer((input_size, layer_sizes[0])), activation_list[0]()]
     for layer_number in range(1, len(layer_sizes)):
         current_layer_size = (layer_sizes[layer_number - 1], layer_sizes[layer_number])
         layers_list.append(FullyConnectedLayer(current_layer_size))
@@ -79,7 +77,9 @@ def main():
     y = y.numpy()
     x = x.reshape(x.shape[0], -1)
     x = np.rollaxis(x, axis=1)
-    x, _, _ = standardize(x)
+    # x, _, _ = standardize(x)
+    x = squish_range(x)
+    x = normalize(x, 0.5, 0.5)
 
     y = make_one_hot_vector(y, classes=10)
     y = np.rollaxis(y, axis=1)
@@ -94,7 +94,7 @@ def main():
     optimizer = SGD(layers=model.layers_list, learning_rate=learning_rate)
 
     batch_size = 4096
-    epochs = 1000  # Irrelevant to RL
+    epochs = 20  # Irrelevant to RL
     train(x, y, model=model, epochs=epochs, batch_size=batch_size, optimizer=optimizer, do_grad_check=False)
 
 

src/ConvNet/torch_tutorial/example.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 import torchvision
 import torchvision.transforms as transforms
 from tqdm import tqdm
 
 DATA_ROOT = r'F:\My Documents\Study\Programming\PycharmProjects\Reinforcement-Learning\data'
-
 
 
 class Net(nn.Module):
@@ -21,6 +21,7 @@ class Net(nn.Module):
         # self.fc3 = nn.Linear(84, 10)
         self.fc1 = nn.Linear(28 * 28, 20)
         self.fc2 = nn.Linear(20, 10)
+        self.activation = nn.LogSoftmax()
 
     def forward(self, x):
         # x = self.pool(F.relu(self.conv1(x)))
@@ -29,13 +30,9 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         # x = F.relu(self.fc2(x))
         x = self.fc2(x)
-        x = F.softmax(x)
+        # x = self.activation(x)
         return x
 
-
-
-
-import torch.optim as optim
 
 
 if __name__ == '__main__':
@@ -67,22 +64,22 @@ if __name__ == '__main__':
 
     net = Net()
 
-    # criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss(reduction='mean')
     # criterion = nn.NLLLoss(reduction='mean')
-    criterion = nn.MSELoss(reduction='mean')
-    optimizer = optim.SGD(net.parameters(), lr=1)
+    # criterion = nn.MSELoss(reduction='mean')
+    optimizer = optim.SGD(net.parameters(), lr=0.1)
 
     # Train
     for epoch in range(10):  # loop over the dataset multiple times
 
         running_loss = 0.0
         pbar = tqdm(enumerate(trainloader, 0))
-        for i, data in pbar:
+        for batch_number, data in pbar:
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
             inputs = inputs.reshape(-1, 784)
-            labels = F.one_hot(labels)
-            labels = labels.float()
+            # labels = F.one_hot(labels)
+            # labels = labels.float()
             # zero the parameter gradients
             optimizer.zero_grad()
 
@@ -93,12 +90,15 @@ if __name__ == '__main__':
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
-            a_labels = torch.argmax(labels, axis=1)
+            current_loss = loss.item()
+            running_loss += current_loss
+            # a_labels = torch.argmax(labels, axis=1)
+            a_labels = labels
             accuracy = torch.mean(
                 torch.as_tensor(a_labels == torch.argmax(outputs, axis=1), dtype=torch.float)).numpy()
 
-            pbar.desc = '[%d, %5d] loss: %.3f,accuracy %.f' % (epoch + 1, i + 1, loss.item(), accuracy * 100)
+            # pbar.desc = '[%d, %5d] loss: %.3f,accuracy %.f' % (epoch + 1, batch_number + 1, loss.item(), accuracy * 100)
+            pbar.desc = f'[{epoch + 1:3d},{batch_number + 1:3d}];  loss {current_loss:.3f}; accuracy {accuracy * 100:.3f}'
 
             running_loss = 0.0
 

src/ConvNet/utils.py
@@ -7,6 +7,12 @@ def rms(x, ax=None, kdims=False):
     return y
 
 
+def squish_range(x):
+    x = x - np.min(x)
+    x = x / np.max(x)
+    return x
+
+
 def normalize(x, mean, std):
     x = (x - mean) / std
     return x
@@ -34,7 +40,7 @@ def grad_check(model, x_batch, y_batch):
     eps = 1e-6
     i = 2
     j = 1
-    L = -2
+    L = -4
     model.layers_list[L].w[i, j] -= eps
     y_pred_1 = model(x_batch)
     model.layers_list[L].w[i, j] += eps
@@ -44,8 +50,7 @@ def grad_check(model, x_batch, y_batch):
     dw_approx = (cost2 - cost1) / (eps)
     dw_net = model.layers_list[L].dw[i, j]
     error = (dw_approx - dw_net) / (np.abs(dw_approx) + np.abs(dw_net) + eps)
-    print(f'dw aprx {dw_approx}; dw net {dw_net}')
-    print('grad check error {:1.3f}%'.format(error * 100))
+    print(f'dw aprx {dw_approx:3.3f}; dw net {dw_net:3.3f}; grad check error {error * 100:1.1f}%')
     return dw_approx
 
 
@@ -82,8 +87,8 @@ def train(x, y, model, epochs, optimizer, batch_size=None, do_grad_check=False):
 
             a_y_pred = np.argmax(y_pred, axis=0)
             a_y_true = np.argmax(y_batch, axis=0)
-            accuracy = np.mean(a_y_true == a_y_pred, axis=0) * 100
-            pbar.desc = f'[{epoch:3d},{batch_number + 1:3d}];  loss {current_loss:.3f}; accuracy {accuracy:.3f}; learning_rate {optimizer.learning_rate}'
+            accuracy = np.mean(a_y_true == a_y_pred, axis=0)
+            pbar.desc = f'[{epoch + 1:3d},{batch_number + 1:3d}];  loss {current_loss:.3f}; accuracy {accuracy * 100:.3f}; learning_rate {optimizer.learning_rate}'
 
         # end batches
         last_cost_mean = np.mean(loss_list[-batches:])